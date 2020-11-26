3c09784a3612d86b3d174a7113e90a763327ae96
Dimitri Lavrenük <dimitri.lavrenuek@camunda.com>
2020-11-26 09:17:57 +0100

    some test

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 export IAM_RELEASE="v1.2.123"
 
-if ! [[ $IAM_RELEASE =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ $IAM_RELEASE =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo $IAM_RELEASE;
-  echo "wrong format";
+  echo "onprem format";
+fi
+
+if [[ $IAM_RELEASE =~ ^v[0-9]$ ]]; then
+  echo $IAM_RELEASE;
+  echo "saas format";
 fi
