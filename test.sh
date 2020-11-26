d0b29c111567a62a024b7852510252ceabb205aa
Dimitri Lavrenük <dimitri.lavrenuek@camunda.com>
2020-11-26 14:53:59 +0100

    change ci

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-export IAM_RELEASE="v1"
+export IAM_RELEASE="v1.1.1-ALPHA"
 
-if [[ $IAM_RELEASE =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ $IAM_RELEASE =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Z0-9]+){0,1}$ ]]; then
   echo $IAM_RELEASE;
   echo "onprem format";
 fi
